eda93944a36c4f361ef00084891e277cb1ecb710
Michael Higgins <mpatrickh7@gmail.com>
2014-07-17 22:38:01 -0500

    Print Different Variable Types
    I printed different variable types to the console. You can't print
    different variable types in the same Console.WriteLine().

--- Hello_World.cs
+++ Hello_World.cs
@@ -9,8 +9,27 @@ namespace Hello_World
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! :D");
-            Console.ReadLine();
+            int myInt = 42;
+            float daFloat = 98.6f;
+            double doubleTrouble = 12345.6789;
+            char zzzzz = 'Z';
+            string myString = "The quick brown fox jumped over the lazy dogs.\n";
+            Console.WriteLine(myInt);
+            Console.WriteLine(daFloat);
+            Console.WriteLine(doubleTrouble);
+            Console.WriteLine(zzzzz);
+            Console.WriteLine(myString);
+
+            myInt = 25;
+            daFloat = 100.3f;
+            doubleTrouble = 98765.4321;
+            zzzzz = 'M';
+            myString = "A quick movement of the enemy will jeopardize six gun boats";
+            Console.WriteLine(myInt);
+            Console.WriteLine(daFloat);
+            Console.WriteLine(doubleTrouble);
+            Console.WriteLine(zzzzz);
+            Console.WriteLine(myString);
         }
     }
 }
