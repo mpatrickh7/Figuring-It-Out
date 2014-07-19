ac4714916a676c0533f6a1ee2f6a1773280c94b4
Michael Higgins <mpatrickh7@gmail.com>
2014-07-18 21:46:12 -0500

    Conditional Operator
    In this exercise I use the ternary operator, ( ? : )

--- Hello_World.cs
+++ Hello_World.cs
@@ -10,18 +10,14 @@ namespace Hello_World
         
         static void Main(string[] args)
         {
-            const int minimum = 100;
-            const int maximum = 300;
-            const int Abby = 135;
-            const int Bob = 175;
-            const int Charlie = 55;
-            const int Dawn = 45;
+            const double radius = 5;
+            const float Pi = 3.14159f;
+            double surfaceArea = (4 * Pi * radius) * 2;
+            double volume = (4 / 3) * (Pi * radius) * 3;
+            double maxValue = surfaceArea > volume ? surfaceArea : volume;
+            Console.WriteLine("surfaceArea: {0}, \nvolume: {1}, \nmaxValue: {2}", surfaceArea, volume, maxValue);
+            Console.WriteLine("The greater of these is {0}", maxValue);
             
-
-
-            Console.WriteLine("Abby and Bob can ride? {0}", (Abby + Bob > minimum) && (Abby + Bob <= maximum));
-            Console.WriteLine("Bob and Charlie can ride? {0}", (Bob + Charlie > minimum) && (Bob + Charlie <= maximum));
-            Console.WriteLine("Charlie and Dawn can ride? {0}", (Charlie + Dawn > minimum) & (Charlie + Dawn <= maximum));
         }
     }
 }
