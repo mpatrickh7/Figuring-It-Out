40880278d48f03f523745dd1189ea9ea386f1f9a
Michael Higgins <mpatrickh7@gmail.com>
2014-07-18 21:20:01 -0500

    Condition Statement
    Add two variables and output whether the condition is true or false
    based on the sum of the variables.

--- Hello_World.cs
+++ Hello_World.cs
@@ -10,18 +10,18 @@ namespace Hello_World
         
         static void Main(string[] args)
         {
-            int originalNumber = 22;
-            int result;
-            int nextNumber = 22;
-            int result2;
+            const int minimum = 100;
+            const int maximum = 300;
+            const int Abby = 135;
+            const int Bob = 175;
+            const int Charlie = 55;
+            const int Dawn = 45;
+            
 
-            //int nextNumber = originalNumber++;
 
-            result = ++originalNumber; // This is taking 1, represented by '++,' and adding it to 22, represented by originalNumber. 1 + 22 = 23
-            result2 = nextNumber++; // result2 automatically equals the value of nextNumber which is 22. nextNumber is saying "I'm 22... now add 1 to me." Which is 23
-
-            Console.WriteLine("Prefix: ++originalNumber = {0}, result = {1}", originalNumber, result);
-            Console.WriteLine("Postfix: nextNumber++ = {0}, result2 = {1}", nextNumber, result2);
+            Console.WriteLine("Abby and Bob can ride? {0}", (Abby + Bob > minimum) && (Abby + Bob <= maximum));
+            Console.WriteLine("Bob and Charlie can ride? {0}", (Bob + Charlie > minimum) && (Bob + Charlie <= maximum));
+            Console.WriteLine("Charlie and Dawn can ride? {0}", (Charlie + Dawn > minimum) & (Charlie + Dawn <= maximum));
         }
     }
 }
